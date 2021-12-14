f19926593dfe8480dd9ec54f640e0bbfe6f5d5a0
Raouf Satto <raoufsato999@gmail.com>
2021-12-14 23:05:57 +0300

    handling connect to db after error

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const { DB_NAME, DB_USERNAME, DB_PASSWORD } = require("./config/config");
 const app = express();
 const port = 3000;
 
+const connectWithRetry = () => {
+	mongoose
+		.connect(
+			`${DB_NAME}://${DB_USERNAME}:${DB_PASSWORD}@${DB_NAME}:27017/?authSource=admin`
+		)
+		.then(() => console.log("Successfully connected to db"))
+		.catch((e) => {
+			console.log(e);
+			setTimeout(connectWithRetry, 5000);
+		});
+};
+connectWithRetry();
 app.get("/", (req, res) => res.send("Hello World!sda"));
 
-mongoose
-	.connect(
-		`${DB_NAME}://${DB_USERNAME}:${DB_PASSWORD}@${DB_NAME}:27017/?authSource=admin`
-	)
-	.then(() => console.log("Successfully connected to db"))
-	.catch((e) => console.log(e));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
